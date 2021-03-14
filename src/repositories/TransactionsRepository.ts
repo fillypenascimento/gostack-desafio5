7bf7a3bb326c383ce564924648b483084e95f1d7
Fillype Nascimento <fillypealna@gmail.com>
2021-03-14 01:31:17 -0300

    Finalizado desafio 3, testes passando.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,12 @@
+import { v4 as uuid } from 'uuid';
+
 import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -14,15 +22,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 
   public getBalance(): Balance {
-    // TODO
-  }
+    const finalBalance: Balance = this.transactions.reduce(
+      (balance: Balance, transaction: Transaction) => {
+        const bal = balance; // https://stackoverflow.com/questions/54274004/why-am-i-getting-a-assignment-to-property-of-function-parameter-eslinterror
 
-  public create(): Transaction {
-    // TODO
+        if (transaction.type === 'income') {
+          bal.income += transaction.value;
+          bal.total += transaction.value;
+        } else if (transaction.type === 'outcome') {
+          bal.outcome += transaction.value;
+          bal.total -= transaction.value;
+        }
+
+        return bal;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return finalBalance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Invalid transaction type.');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new Error(
+        'This transaction could not be executed. The value available is not enough.',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
